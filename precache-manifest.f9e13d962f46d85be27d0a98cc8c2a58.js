44613500add5f122c1ab580c286bf93408e194eb
Makoto Inoue <2630+makoto@users.noreply.github.com>
2020-08-18 15:11:12 +0100

    Updates

---
+++ precache-manifest.f9e13d962f46d85be27d0a98cc8c2a58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d80be726ecd20b9291dba3e3b5a482e",
+    "url": "/claim-deposit/index.html"
+  },
+  {
+    "revision": "98342ec35163d6f4bab5",
+    "url": "/claim-deposit/static/css/main.96162d31.chunk.css"
+  },
+  {
+    "revision": "f5e5b7218b2c257f54d4",
+    "url": "/claim-deposit/static/js/2.93df4686.chunk.js"
+  },
+  {
+    "revision": "5fe065d6b23c0975d7c5b049d6834459",
+    "url": "/claim-deposit/static/js/2.93df4686.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "98342ec35163d6f4bab5",
+    "url": "/claim-deposit/static/js/main.38b70c66.chunk.js"
+  },
+  {
+    "revision": "251376746019391219a7",
+    "url": "/claim-deposit/static/js/runtime-main.81c70be4.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.759285a976d82ca2f49495929df3de18.js"
+  "/claim-deposit/precache-manifest.f9e13d962f46d85be27d0a98cc8c2a58.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/claim-deposit/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
