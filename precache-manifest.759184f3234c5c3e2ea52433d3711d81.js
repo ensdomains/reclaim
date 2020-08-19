5f87bb353f748ad433e5542303536bf56f0849bc
Makoto Inoue <2630+makoto@users.noreply.github.com>
2020-08-19 22:22:17 +0100

    Updates

---
+++ precache-manifest.759184f3234c5c3e2ea52433d3711d81.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "edf97dab9d76546f0ae869151aeb89f6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b266798039a776b853ec",
+    "url": "/static/css/main.e9aedb98.chunk.css"
+  },
+  {
+    "revision": "1ad5b20ea022f2de444c",
+    "url": "/static/js/2.f4740e0d.chunk.js"
+  },
+  {
+    "revision": "bd1ffdb02956310b21fba67384832a94",
+    "url": "/static/js/2.f4740e0d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b266798039a776b853ec",
+    "url": "/static/js/main.65fb7a23.chunk.js"
+  },
+  {
+    "revision": "8f96027f25cd7b7e59d5",
+    "url": "/static/js/runtime-main.db7e2936.js"
+  }
+]);
